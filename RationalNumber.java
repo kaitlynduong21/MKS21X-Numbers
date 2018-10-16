f9dfd34180627dd40c6854a9dd9523f2983ca9e5
kduong@stuy.edu <kduong@stuy.edu>
2018-10-15 22:09:19 -0400

    Added constructor methods, accessor methods, toString and compareTo

---
+++ RationalNumber.java
@@ -0,0 +1,28 @@
+public class RationalNumber extends RealNumber{
+  private int num;
+  private int denom;
+
+  public RationalNumber (int x, int y) {
+  //constructor method
+  }
+
+  public RationalNumber () {
+  //constructor method
+  }
+
+  public double getValue () {
+    //accessor method
+  }
+
+  public String toString() {
+    //toString method that overrides the other toString
+  }
+
+  public double compareTo(RationalNumber a, RationalNumber b) {
+    //comparing two rational numbers
+  }
+
+  public double compareToReal(RationalNumber a, RealNumber b) {
+    //comparing a real number to a rational number
+  }
+}
---
+++ RealNumber.java
@@ -0,0 +1,22 @@
+public class RealNumber{
+
+  private double num;
+
+  public RealNumber () {
+    //constructor default method
+  }
+
+  public RealNumber(double x) {
+    //constructor method
+  }
+
+  public double getValue() {
+    //accessor method
+  }
+
+  public String toString() {
+    //toString method
+  }
+
+  public double compareTo(RealNumber a, RealNumber b)
+}
